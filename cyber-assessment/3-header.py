c8da1da8d688405bdce4c4aa7d3ade355e9dcb69
nargiznaghi <nargiznaghiyeva10@gmail.com>
2026-03-23 11:46:55 +0100

    Update 3-header.py

--- cyber-assessment/3-header.py
+++ cyber-assessment/3-header.py
@@ -2,7 +2,5 @@
 import urllib.request
 import sys
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:
-        print(response.headers.get("X-Request-Id"))
+with urllib.request.urlopen(sys.argv[1]) as response:
+    print(response.headers.get("X-Request-Id"))
