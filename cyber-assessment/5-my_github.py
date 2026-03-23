7514acaf21e1ec8cdc3c49cd7cd2df004e05124f
nargiznaghi <nargiznaghiyeva10@gmail.com>
2026-03-23 18:01:22 +0100

    5-my_github.py

--- cyber-assessment/5-my_github.py
+++ cyber-assessment/5-my_github.py
@@ -8,7 +8,4 @@ token = sys.argv[2]
 url = "https://api.github.com/user"
 response = requests.get(url, auth=(username, token))
 
-if response.status_code == 200:
-    print(response.json().get("id"))
-else:
-    print("None")
+print(response.json().get("id"))
