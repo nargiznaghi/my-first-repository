9de3acf43fec15b4a70c68a174161d48dc353a8d
nargiznaghi <nargiznaghiyeva10@gmail.com>
2026-03-23 13:00:30 +0100

    5-my_github.py

--- cyber-assessment/5-my_github.py
+++ cyber-assessment/5-my_github.py
@@ -2,14 +2,13 @@
 import requests
 import sys
 
-if __name__ == "__main__":
-    username = sys.argv[1]
-    token = sys.argv[2]
+username = sys.argv[1]
+token = sys.argv[2]
 
-    url = "https://api.github.com/user"
-    response = requests.get(url, auth=(username, token))
+url = "https://api.github.com/user"
+response = requests.get(url, auth=(username, token))
 
-    try:
-        print(response.json().get("id"))
-    except Exception:
-        print("None")
+if response.status_code == 200:
+    print(response.json().get("id"))
+else:
+    print("None")
